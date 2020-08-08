64943897bd276d2326a79d0e36bd98bbe1104ca8
Tumble <tumblegamer@gmail.com>
2020-08-08 13:00:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mingj-demo
+ 
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log = (...args) => stdout(args.join(" "))
+console.info = (...args) => stdout(args.join(" "),1)
+console.warn = (...args) => stdout(args.join(" "),2)
+console.error = (...args) => stdout(args.join(" "),3)
+
+function startDemo(){
+stdin(GDgetPrompt()).then(input=>{
+	Bash(input);
+	setTimeout(startDemo,0);
+})
+}
+
+startDemo();
