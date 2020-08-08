46b25314997132d6d6a0756f13da583f93ed353a
Tumble <tumblegamer@gmail.com>
2020-08-08 14:08:43 +0100

    intro message

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 console.log = (...args) => stdout(args.join(" "))
-console.info = (...args) => stdout(args.join(" "),1)
-console.warn = (...args) => stdout(args.join(" "),2)
-console.error = (...args) => stdout(args.join(" "),3)
+console.info = (...args) => stdout(args.join(" "), 1)
+console.warn = (...args) => stdout(args.join(" "), 2)
+console.error = (...args) => stdout(args.join(" "), 3)
 
-function startDemo(){
-stdin(GDgetPrompt()).then(input=>{
-	Bash(input);
-	setTimeout(startDemo,0);
-})
+function startDemo() {
+	stdin(GDgetPrompt()).then(input => {
+		Bash(input);
+		setTimeout(startDemo, 0);
+	})
 }
 
-console.log("Type 'help' for a list of commands.")
 startDemo();
